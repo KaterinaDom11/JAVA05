dcee68371ef5b4dcb996972d72210189b61a5218
KaterinaDomasheva <karapetyanmariya11@gmail.com>
2025-02-01 12:57:36 +0300

    init Maven

---
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+
+public class HolidayServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src\\test\\resources\\holiday.csv")
+
+    public void numberOfVacationMonths(int expected, int income, int expenses, int threshold) {
+        HolidayService service = new HolidayService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
+
